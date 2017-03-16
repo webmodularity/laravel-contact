18057154fee9d46e2588b55e739a59463da9c393
Rory Hellyer <rory@webmodularity.com>
2017-03-16 12:19:38 -0700

    add address models

--- src/AddressValidator.php
+++ src/AddressValidator.php
@@ -7,7 +7,7 @@ class AddressValidator
     public function validate($attribute, $value, $parameters, $validator)
     {
         extract($value);
-        dd($validator->hasRule($attribute, 'required'));
+        dd($validator);
         if ($validator->hasRule($attribute, 'required')
             && (
                 empty($street)
