7a2fd395ec7e8f9af9f735bf41461f0df3398833
Rory Hellyer <rory@webmodularity.com>
2017-03-16 15:52:05 -0700

    add address models

--- src/AddressValidator.php
+++ src/AddressValidator.php
@@ -7,28 +7,15 @@ class AddressValidator
     public function validate($attribute, $value, $parameters, $validator)
     {
         extract($value);
+        $isRequired = $validator->hasRule($attribute, 'Required');
+        $rules = [];
 
-        if ($validator->hasRule($attribute, 'Required')
-            && (
-                empty($street)
-                || empty($city)
-                || empty($state_id)
-                || empty($zip)
-            )
-        ) {
-            if (empty($street)) {
-                $validator->errors()->add($attribute . '.street', 'Street Address is required.');
-            }
-            if (empty($city)) {
-                $validator->errors()->add($attribute . '.city', 'City is required.');
-            }
-            if (empty($state_id)) {
-                $validator->errors()->add($attribute . '.state_id', 'State is required.');
-            }
-            if (empty($zip)) {
-                $validator->errors()->add($attribute . '.zip', 'Zip Code is required.');
-            }
-            return false;
+        // Street
+        $rules[$attribute . '.street'] = [
+            'max:255'
+        ];
+        if ($isRequired) {
+            $rules[$attribute . '.street'][] = 'required';
         }
 
         return true;
