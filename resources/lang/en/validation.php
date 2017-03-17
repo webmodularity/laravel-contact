dfd135b243519c1b13edbd22c9a487c5c14632c9
Rory Hellyer <rory@webmodularity.com>
2017-03-17 12:14:56 -0700

    add address models

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    'address' => 'Invalid address format.'
+    'address' => 'Invalid address format.',
+    'street.required' => 'Street is required.',
+    'city.required' => 'City is required.',
+    'state_id.required' => 'State is required',
+    'zip.required' => 'Zip is required'
 ];
