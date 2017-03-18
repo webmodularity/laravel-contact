610dd9436dbdb0eaafdaabed2f8d596e3fa74dde
Rory Hellyer <rory@webmodularity.com>
2017-03-17 18:50:39 -0700

    add address models

--- src/AddressValidator.php
+++ src/AddressValidator.php
@@ -6,39 +6,101 @@ use Validator;
 
 class AddressValidator
 {
+    protected $isRequired = false;
+    protected $attribute;
+    protected $validator;
+
     public function validate($attribute, $value, $parameters, $validator)
     {
-        $isRequired = $validator->hasRule($attribute, 'Required');
+        $hasError = false;
+        $this->isRequired = $validator->hasRule($attribute, 'Required');
+        $this->attribute = $attribute;
+        $this->validator = $validator;
+
+        $street = array_pull($value, 'street');
         $zip = array_pull($value, 'zip');
 
         // Street
-        $streetRules = $isRequired
-            ? 'required|max:255'
-            : 'max:255';
-        // City
-        $cityRules = $isRequired
-            ? 'required|max:100'
-            : 'max:100';
-        // State
-        $stateRules = $isRequired
-            ? 'required|integer|exists:address_states,id'
-            : 'integer|exists:address_states,id';
+        $streetRules = [
+            'max:255'
+        ];
+        if ($this->isRequired) {
+            array_unshift($streetRules, 'required');
+        }
+        // Validate Street
+        $streetValidator = Validator::make($street, [
+            'street' => $streetRules
+        ]);
 
+        if ($streetValidator->fails()) {
+            $this->transferErrors($streetValidator->errors()->getMessages());
+            $hasError = true;
+        } elseif (is_null($street['street'])) {
+            // Address is optional and street is null, ignore address
+            return true;
+        }
+
+        // City
+        $cityRules = [
+            'max:100'
+        ];
+        if ($this->isRequired) {
+            array_unshift($cityRules, 'required');
+        }
+        // State
+        $stateRules = [
+            'integer',
+            'exists:address_states,id'
+        ];
+        if ($this->isRequired) {
+            array_unshift($stateRules, 'required');
+        }
+        // Validate City & State
         $addressValidator = Validator::make($value, [
-            'street' => $streetRules,
             'city' => $cityRules,
             'state_id' => $stateRules
         ]);
 
         if ($addressValidator->fails()) {
-            foreach ($addressValidator->errors()->getMessages() as $key => $errors) {
-                foreach ($errors as $error) {
-                    $validator->errors()->add($attribute . '.' . $key, $error);
-                }
-            }
-            return false;
+            $this->transferErrors($addressValidator->errors()->getMessages());
+            $hasError = true;
         }
 
-        return false;
+        // Zip
+        $zipRules = [
+            'max:20'
+        ];
+        if (!is_null($value['state_id']) && $this->validator->errors()->has($this->attribute . '.' . 'state_id')) {
+            $postalRegex = AddressState::select('postal_regex')
+                ->leftJoin('address_countries', 'address_states.country_id', '=', 'address_countries.id')
+                ->where('address_states.id', $value['state_id'])
+                ->first();
+            if (!is_null($postalRegex)) {
+                array_unshift($zipRules, 'regex:/' . $postalRegex->postal_regex . '/');
+            }
+        }
+        if ($this->isRequired) {
+            array_unshift($zipRules, 'required');
+        }
+        // Validate Zip
+        $zipValidator = Validator::make($zip, [
+            'zip' => $zipRules
+        ]);
+
+        if ($zipValidator->fails()) {
+            $this->transferErrors($zipValidator->errors()->getMessages());
+            $hasError = true;
+        }
+
+        return ! $hasError;
+    }
+
+    protected function transferErrors($errors)
+    {
+        foreach ($errors as $key => $allErrors) {
+            foreach ($allErrors as $error) {
+                $this->validator->errors()->add($this->attribute . '.' . $key, $error);
+            }
+        }
     }
 }
